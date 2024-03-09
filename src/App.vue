3d2ca72f268e0d4dc9074ff0840e089ff00fb152
jakemackie <jake.wmackie@gmail.com>
2024-03-09 22:16:39 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="min-h-screen bg-gray-100 grid place-items-center">
+    <span>
+      <!-- Vue -->
+      <h1 class="text-4xl">Vue</h1>
+      <a
+        class="text-green-500"
+        href="https://v3.vuejs.org/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <code>(v3.0.0)</code>
+      </a>
+
+      <!-- Tailwind -->
+      <h1 class="text-4xl">Tailwind CSS</h1>
+      <a
+        class="text-blue-500"
+        href="https://tailwindcss.com/blog/tailwindcss-v4-alpha"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <code>(Alpha v4.0.0)</code>
+      </a>
+    </span>
+  </div>
+</template>
